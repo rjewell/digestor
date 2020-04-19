e22d587cebfba77e5a106ad53668cec6bb6e5eb3
Bob Jewell <bob@disclosed.org>
2020-04-19 15:52:24 -0400

    Created initial EF model

--- DigestorApi/Models/Message.cs
+++ DigestorApi/Models/Message.cs
@@ -1,10 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Message
 {
+    public Message()
+    {
+        Sent = DateTime.Now;
+    }
     public long Id { get; set; }
     public int ChatId { get; set; }
-    public DateTime Sent { get; set; }
+
+    public DateTime Sent{ get; set; }
     public string Sender { get; set; }
     public string Content { get; set; }
 }
